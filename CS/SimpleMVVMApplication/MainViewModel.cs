5193d3453a618c932208a2a7110992b49006f215
Anthony Sameishchev <anton.sameishchev@devexpress.com>
2022-06-03 09:56:00 +0400

    code added

---
+++ CS/SimpleMVVMApplication/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Mvvm.CodeGenerators;
+using DevExpress.Mvvm;
+
+namespace SimpleMVVMApplication {
+    [GenerateViewModel(ImplementISupportUIServices = true)]
+    public partial class MainViewModel {
+        void OnUserNameChanged() { RegisterCommand.RaiseCanExecuteChanged(); }
+        bool CanRegister() => !string.IsNullOrEmpty(UserName);
+
+        [GenerateProperty]
+        string userName;
+        IMessageBoxService MessageBoxService { get => ServiceContainer.GetService<IMessageBoxService>(); }
+
+        [GenerateCommand]
+        void Register() { MessageBoxService.ShowAsync($"{UserName} was successfully registered!", "Registration"); }
+    }
+}
